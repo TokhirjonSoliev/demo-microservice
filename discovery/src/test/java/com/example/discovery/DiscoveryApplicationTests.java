0226078e9c598bbb5abe8da41dbc0677ad3f2de1
tokhirjon <tokhirjon007@gmail.com>
2023-01-23 12:50:30 +0500

    minor changes

--- discovery/src/test/java/com/example/discovery/DiscoveryApplicationTests.java
+++ discovery/src/test/java/com/example/discovery/DiscoveryApplicationTests.java
@@ -1,13 +1,7 @@
 package com.example.discovery;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DiscoveryApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
 }
--- user-service/src/test/java/com/example/demo1/Demo1ApplicationTests.java
+++ user-service/src/test/java/com/example/demo1/Demo1ApplicationTests.java
@@ -1,33 +1,7 @@
 package com.example.demo1;
 
-import com.example.demo1.dto.UserCreateDto;
-import com.example.demo1.dto.UserUpdateDto;
-import com.example.demo1.entity.User;
-import com.example.demo1.repository.UserRepository;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
-
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 class Demo1ApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 }
