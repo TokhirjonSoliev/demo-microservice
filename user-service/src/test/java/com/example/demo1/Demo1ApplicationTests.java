2e03a2db5a18031f2b92b481913eb19e3238efea
tokhirjon <tokhirjon007@gmail.com>
2023-01-23 12:40:33 +0500

    minor changes

--- user-service/src/test/java/com/example/demo1/Demo1ApplicationTests.java
+++ user-service/src/test/java/com/example/demo1/Demo1ApplicationTests.java
@@ -25,91 +25,9 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
-@Testcontainers
-@ExtendWith(SpringExtension.class)
-@AutoConfigureMockMvc
 class Demo1ApplicationTests {
-    @Autowired
-    private ObjectMapper objectMapper;
-    @Autowired
-    private MockMvc mockMvc;
-    @Autowired
-    private UserRepository userRepository;
-    @Container
-    static PostgreSQLContainer<?> database = new PostgreSQLContainer<>("postgres:12.3")
-            .withDatabaseName("test")
-            .withUsername("test")
-            .withPassword("test");
-
-    @DynamicPropertySource
-    static void properties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", database::getJdbcUrl);
-        registry.add("spring.datasource.password", database::getPassword);
-        registry.add("spring.datasource.username", database::getUsername);
-    }
 
     @Test
-    void shouldCreateUser() throws Exception {
-        UserCreateDto userCreateDto = getUserCreateDto();
-        String userCreateDtoSting = objectMapper.writeValueAsString(userCreateDto);
-        MvcResult mvcResult = checkingUserCreation(userCreateDtoSting);
-        User user = objectMapper.readValue(mvcResult.getResponse().getContentAsString(), User.class);
-        Assertions.assertEquals("John", userRepository.findById(user.getId()).get().getFirstName());
-        userRepository.delete(user);
+    void contextLoads() {
     }
-
-    @Test
-    void shouldEditUser() throws Exception {
-        UserCreateDto userCreateDto = getUserCreateDto();
-        UserUpdateDto userUpdateDto = getUserUpdateDto();
-        String userCreateDtoSting = objectMapper.writeValueAsString(userCreateDto);
-        String userUpdateDtoString = objectMapper.writeValueAsString(userUpdateDto);
-        MvcResult mvcResult = checkingUserCreation(userCreateDtoSting);
-        User user = objectMapper.readValue(mvcResult.getResponse().getContentAsString(), User.class);
-        MvcResult updatedMvcResult = mockMvc.perform(MockMvcRequestBuilders.put("/api/user/edit/" + user.getId())
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(userUpdateDtoString))
-                .andExpect(status().isOk())
-                .andReturn();
-        User updatedUser = objectMapper.readValue(updatedMvcResult.getResponse().getContentAsString(), User.class);
-        Assertions.assertEquals("Tom", userRepository.findById(updatedUser.getId()).get().getFirstName());
-        userRepository.delete(user);
-    }
-
-    @Test
-    void shouldDeleteUser() throws Exception {
-        UserCreateDto userCreateDto = getUserCreateDto();
-        String userCreateDtoSting = objectMapper.writeValueAsString(userCreateDto);
-        MvcResult mvcResult = checkingUserCreation(userCreateDtoSting);
-        User user = objectMapper.readValue(mvcResult.getResponse().getContentAsString(), User.class);
-        mockMvc.perform(MockMvcRequestBuilders.delete("/api/user/delete/" + user.getId()))
-                .andExpect(status().isOk());
-        Assertions.assertEquals(0, userRepository.findAll().size());
-    }
-
-    private MvcResult checkingUserCreation(String userCreateDtoSting) throws Exception {
-        return mockMvc.perform(MockMvcRequestBuilders.post("/api/user/new")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(userCreateDtoSting))
-                .andExpect(status().isCreated())
-                .andReturn();
-    }
-
-
-    private UserCreateDto getUserCreateDto() {
-        return UserCreateDto.builder()
-                .firstName("John")
-                .lastName("Terry")
-                .username("john_st")
-                .email("john@gmail.com")
-                .password("johnterry")
-                .build();
-    }
-
-    private UserUpdateDto getUserUpdateDto() {
-        return UserUpdateDto.builder()
-                .firstName("Tom")
-                .build();
-    }
-
 }
